1fce6db094d13554f5fb52d140987b3d15f113db
wando <kdw40161217@gmail.com>
2023-04-18 22:11:43 +0900

    feat: json transfer #4

--- main.go
+++ main.go
@@ -1,22 +1,53 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
+	"time"
 )
+
+type User struct {
+	FirstName string    `json:"first_name"`
+	LastName  string    `json:"last_name"`
+	Email     string    `json:"email"`
+	CreatedAt time.Time `json:"created_at"`
+}
 
 type wandoHandler struct{}
 
 func (wa *wandoHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "Wando Handler")
+	user := new(User)
+	err := json.NewDecoder(r.Body).Decode(user)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		fmt.Fprint(w, err)
+		return
+	}
+	user.CreatedAt = time.Now()
+	data, _ := json.Marshal(user) // json 형태로 encoding
+	w.Header().Add("content-type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprint(w, string(data))
+}
+func minjuHandler(w http.ResponseWriter, r *http.Request) {
+	name := r.URL.Query().Get("name")
+	if name == "" {
+		name = "World"
+	}
+	fmt.Fprintf(w, "Minju %s", name)
 }
 
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprint(w, "Hello World")
 	})
 
-	http.Handle("/wando", &wandoHandler{})
+	mux.HandleFunc("/minju", minjuHandler)
 
-	http.ListenAndServe(":3000", nil)
+	mux.Handle("/wando", &wandoHandler{})
+
+	http.ListenAndServe(":3000", mux)
 }
